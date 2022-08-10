5925e2d82b6a83b1ef31bc0c203ab0a5e324caf6
Mohammad Arif <arif_iq@yahoo.co.in>
2022-08-10 21:48:46 +0530

    Add examples of add and remove shopping cart items

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ import useLocalStorage from './CustomHooks/useLocalStorage'
 import useUpdateLogger from './CustomHooks/useUpdateLogger'
 import UseReducer from './components/UseReducer'
 import Counter from './components/Counter'
+import ShoppingCart from './components/UpdateShoppingCartItems'
+// import ShoppingCart from './components/RemoveShoppingCartItems'
 
 function App () {
   // const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
   // return <NumberList numbers={numbers} />
-  return <Counter />
+  // return <Counter />
+  return <ShoppingCart />
   /*   const [name, setName] = useLocalStorage('name', '')
   useUpdateLogger(name)
   return (
---
+++ src/components/RemoveShoppingCartItems.js
@@ -0,0 +1,94 @@
+import { useState } from 'react'
+
+/**
+ * Challenge 2 of 4: Remove an item from the shopping cart
+ *
+ * This shopping cart has a working ”+” button, but the ”–” button doesn’t do anything.
+ * You need to add an event handler to it so that pressing it decreases the count of
+ * the corresponding product.
+ *
+ * If you press ”–” when the count is 1, the product should
+ * automatically get removed from the cart. Make sure it never shows 0.
+ */
+
+const initialProducts = [
+  {
+    id: 0,
+    name: 'Baklava',
+    count: 2
+  },
+  {
+    id: 1,
+    name: 'Cheese',
+    count: 5
+  },
+  {
+    id: 2,
+    name: 'Spaghetti',
+    count: 2
+  }
+]
+
+export default function ShoppingCart () {
+  const [products, setProducts] = useState(initialProducts)
+
+  function handleIncreaseClick (productId) {
+    setProducts(
+      products.map(product => {
+        if (product.id === productId) {
+          return {
+            ...product,
+            count: product.count + 1
+          }
+        } else {
+          return product
+        }
+      })
+    )
+  }
+
+  function handleDecreaseClick (productId) {
+    /**
+     * You can first use map to produce a new array,
+     * and then filter to remove products with a count set to 0:
+     */
+    let nextProducts = products.map(product => {
+      if (product.id === productId) {
+        return {
+          ...product,
+          count: product.count - 1
+        }
+      } else {
+        return product
+      }
+    })
+
+    nextProducts = nextProducts.filter(p => p.count > 0)
+
+    setProducts(nextProducts)
+  }
+
+  return (
+    <ul>
+      {products.map(product => (
+        <li key={product.id}>
+          {product.name} (<b>{product.count}</b>)
+          <button
+            onClick={() => {
+              handleIncreaseClick(product.id)
+            }}
+          >
+            +
+          </button>
+          <button
+            onClick={() => {
+              handleDecreaseClick(product.id)
+            }}
+          >
+            –
+          </button>
+        </li>
+      ))}
+    </ul>
+  )
+}
