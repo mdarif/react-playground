d7bfe61846939447da2fb200b825d6ac0d1b4dd1
Mohammad Arif <arif_iq@yahoo.co.in>
2022-10-28 19:08:06 +0530

    API Key & Secret is out from the code, default port is also configured

--- src/components/FetchAPI/proxyFetch.js
+++ src/components/FetchAPI/proxyFetch.js
@@ -2,44 +2,43 @@ import { useEffect, useState } from "react";
 
 export default function ProxyFetch() {
   const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   async function loadData() {
     try {
+      setLoading(true);
+      const API_KEY = process.env.REACT_APP_API_KEY;
+      const API_SECRET = process.env.REACT_APP_API_SECRET;
       const response = await fetch("/v1/products/", {
         method: "get",
         headers: {
           "Content-Type": "application/json",
-          Authorization:
-            "Basic " +
-            window.btoa(
-              "6d0480d5-2132-56bb-9f04-fec613de2ac4" +
-                ":" +
-                "fa838754-c2e0-5118-85ba-be8b835b0774"
-            ),
+          Authorization: "Basic " + window.btoa(API_KEY + ":" + API_SECRET),
         },
       });
       const data = await response.json();
       setData(data);
       setLoading(false);
     } catch (error) {
-      console.log(error);
+      setError(error.message);
+      setLoading(false);
     }
   }
 
-  useEffect(() => {
-    loadData();
-  }, []);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  const mappedData = data.map((item) => <li key={item.id}>{item.name}</li>);
+  const mappedData =
+    data && data.map((item) => <li key={item.id}>{item.name}</li>);
 
   return (
     <>
-      <ul>{!loading && mappedData}</ul>
+      <div>
+        <button onClick={loadData}>Load Data via Proxy</button>{" "}
+        <button onClick={() => setData(null)}>Clear Data</button>
+      </div>
+
+      {loading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+      <ul>{!error && mappedData}</ul>
     </>
   );
 }
